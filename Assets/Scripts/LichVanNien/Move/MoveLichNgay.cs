b67d7194e98f314e83f783bd3fa812c44e759e99
Huy Vu <dinhthitampro@gmail.com>
2018-02-26 23:29:21 +0700

    Mo Rong Hom nay
    today

---
+++ Assets/Scripts/LichVanNien/Move/MoveLichNgay.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLichNgay : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            // Get movement of the finger since last frame
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+
+            // Move object across XY plane
+            transform.Translate(touchDeltaPosition.x * speed, this.transform.position.y, 0);
+        }
+        else if (Input.touchCount != 0 && Input.GetTouch(0).phase
+       == TouchPhase.Ended)
+        {
+            
+        }
+
+
+	}
+}
