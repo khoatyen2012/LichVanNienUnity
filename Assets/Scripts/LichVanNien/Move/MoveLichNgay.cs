b95bcf6075fabe73e97db88fa3c0b0d921ef3a97
Huy Vu <dinhthitampro@gmail.com>
2018-02-28 23:01:36 +0700

    Hoan Thanh Update mau
    Update mau

--- Assets/Scripts/LichVanNien/Move/MoveLichNgay.cs
+++ Assets/Scripts/LichVanNien/Move/MoveLichNgay.cs
@@ -53,38 +53,50 @@ public class MoveLichNgay : MonoBehaviour {
 		switch (pDt.DayOfWeek.ToString ().ToLower ().Substring(0,2)) {
 		case "mo":
 			pTra.GetChild (0).GetChild (1).GetComponent<tk2dTextMesh> ().text = "Thứ 2";
-			pTra.GetChild (4).GetComponent<tk2dSprite> ().color = new Color ((float)4/255, (float)167/255, (float)12/255, 1);
-			//pTra.GetChild (0).GetChild (2).GetComponent<tk2dTextMesh> ().color
-			//pTra.GetChild (2).GetChild (2).GetComponent<tk2dTextMesh> ().color
-			//pTra.GetChild(2).GetChild(3).GetComponent<tk2dTextMesh>()
+			
+            doUpdateColor(pTra, new Color((float)4 / 255, (float)167 / 255, (float)12 / 255, 1));
 			break;
 		case "tu":
 			pTra.GetChild (0).GetChild (1).GetComponent<tk2dTextMesh> ().text = "Thứ 3";
-			pTra.GetChild (4).GetComponent<tk2dSprite> ().color = new Color ((float)38/255, (float)104/255, (float)234/255, 1);
+			
+            doUpdateColor(pTra, new Color((float)38 / 255, (float)104 / 255, (float)234 / 255, 1));
 			break;
 		case "we":
 			pTra.GetChild (0).GetChild (1).GetComponent<tk2dTextMesh> ().text = "Thứ 4";
-			pTra.GetChild (4).GetComponent<tk2dSprite> ().color = new Color ((float)13/255, (float)126/255, (float)84/255, 1);
+			
+            doUpdateColor(pTra, new Color((float)13 / 255, (float)126 / 255, (float)84 / 255, 1));
 			break;
 		case "th":
 			pTra.GetChild (0).GetChild (1).GetComponent<tk2dTextMesh> ().text = "Thứ 5";
-			pTra.GetChild (4).GetComponent<tk2dSprite> ().color = new Color ((float)205/255, 0, (float)153/255, 1);
+			
+            doUpdateColor(pTra, new Color((float)205 / 255, 0, (float)153 / 255, 1));
 			break;
 		case "fr":
 			pTra.GetChild (0).GetChild (1).GetComponent<tk2dTextMesh> ().text = "Thứ 6";
-			pTra.GetChild (4).GetComponent<tk2dSprite> ().color = new Color ((float)195/255, (float)96/255, (float)52/255, 1);
+			
+            doUpdateColor(pTra, new Color((float)195 / 255, (float)96 / 255, (float)52 / 255, 1));
 			break;
 		case "sa":
 			pTra.GetChild (0).GetChild (1).GetComponent<tk2dTextMesh> ().text = "Thứ 7";
-			pTra.GetChild (4).GetComponent<tk2dSprite> ().color = new Color ((float)38/255, (float)104/255, (float)234/255, 1);
+			
+            doUpdateColor(pTra, new Color((float)38 / 255, (float)104 / 255, (float)234 / 255, 1));
 			break;
 			default:
 			pTra.GetChild (0).GetChild (1).GetComponent<tk2dTextMesh> ().text = "Chủ Nhật";
-			pTra.GetChild (4).GetComponent<tk2dSprite> ().color = new Color (1, (float)84/255, (float)84/255, 1);
+			;
+            doUpdateColor(pTra, new Color(1, (float)84 / 255, (float)84 / 255, 1));
 			break;
 		}
 	
 	}
+
+    public void doUpdateColor(Transform pTra ,Color pColor)
+    {
+        pTra.GetChild(4).GetComponent<tk2dSprite>().color = pColor;
+        pTra.GetChild(0).GetChild(0).GetComponent<tk2dTextMesh>().color = pColor;
+        pTra.GetChild(0).GetChild(1).GetComponent<tk2dTextMesh>().color = pColor;
+        pTra.GetChild(0).GetChild(2).GetComponent<tk2dTextMesh>().color = pColor;
+    }
 	
 	// Update is called once per frame
 	void Update () {
