3f79dff5e6768358e9152acd9c457f796656acaf
Huy Vu <dinhthitampro@gmail.com>
2018-03-07 00:17:11 +0700

    Title Thu
    Dang Lam Do

--- Assets/Scripts/LichVanNien/Move/MoveLichThang.cs
+++ Assets/Scripts/LichVanNien/Move/MoveLichThang.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MoveLichThang : MonoBehaviour {
 
@@ -13,7 +14,53 @@ public class MoveLichThang : MonoBehaviour {
 	public float startY;
 	public float distanceY;
 
-	void CreateItem(float positionX, float positionY, int vitri)
+    private Transform TraTrai;
+    private Transform TraGiua;
+    private Transform TraPhai;
+
+
+    public void setEmptyChild()
+    {
+        var children = new List<GameObject>();
+        foreach (Transform child in TraTrai)
+        {
+            if (child.gameObject.CompareTag("nguoi"))
+            {
+                continue;
+            }
+            children.Add(child.gameObject);
+        }
+
+        foreach (Transform child in TraGiua)
+        {
+            if (child.gameObject.CompareTag("nguoi"))
+            {
+                continue;
+            }
+            children.Add(child.gameObject);
+        }
+
+        foreach (Transform child in TraPhai)
+        {
+            if (child.gameObject.CompareTag("nguoi"))
+            {
+                continue;
+            }
+            children.Add(child.gameObject);
+        }
+
+
+        foreach (GameObject item in children)
+        {
+            item.transform.parent = null;
+            item.transform.Recycle();
+        }
+
+        
+
+    }
+
+	void CreateItem(float positionX, float positionY,Transform pParent)
 	{
 
 		ItemThang levelCreate = itemPrefab.Spawn<ItemThang>
@@ -24,22 +71,22 @@ public class MoveLichThang : MonoBehaviour {
 
 
 
-		levelCreate.transform.parent = this.gameObject.transform.GetChild(1).transform;
+        levelCreate.transform.parent = pParent;
 
 
 	}
 
-	public void Createlevl(int sl)
+    public void Createlevl(int sl, float pStartX, float pStartY,Transform pParent)
 	{
-		float positionCreateX = startX;
-		float positionCreateY = startY;
+        float positionCreateX = pStartX;
+        float positionCreateY = pStartY;
 		for (int i = 1; i <= sl; i++)
 		{
-			CreateItem(positionCreateX, positionCreateY, i);
+            CreateItem(positionCreateX, positionCreateY, pParent);
 			positionCreateX += distanceX;
 			if (i % 7 == 0)
 			{
-				positionCreateX = startX;
+                positionCreateX = pStartX;
 				positionCreateY -= distanceY;
 			}
 		}
@@ -47,14 +94,19 @@ public class MoveLichThang : MonoBehaviour {
 
 	public void setData()
 	{
-		Createlevl (40);
+        setEmptyChild();
+		Createlevl (40,startX,startY,TraGiua);
+        Createlevl(40, startX-580, startY, TraTrai);
+        Createlevl(40, startX+580, startY, TraPhai);
 	}
 
 
 	// Use this for initialization
 	void Start () {
         startPosition = this.transform.position;
-
+        TraTrai = this.gameObject.transform.GetChild(0).transform;
+        TraGiua = this.gameObject.transform.GetChild(1).transform;
+        TraPhai = this.gameObject.transform.GetChild(2).transform;
 	}
 	
 	// Update is called once per frame
